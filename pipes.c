3ad59c6f83d66bfebacf3c40e4617c23eb54924a
Ritesh Ranjan <r.ranjan789@gmail.com>
2014-05-28 00:15:55 +0530

    Pipes with O_NONBLOCK

---
+++ pipes.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include "test.h"
+
+int pcfd[2];	//Parent to child pipe
+
+int main()
+{
+	write(1,"Making pipes\n", 13);
+	if (pipe(pcfd) != 0)
+		perror("Pipe");
+	int flags = fcntl(pcfd[0], F_GETFL);
+	flags |= O_NONBLOCK;
+	fcntl(pcfd[0], F_SETFL, flags);
+
+	flags = fcntl(pcfd[1], F_GETFL);
+	flags |= O_NONBLOCK;
+	fcntl(pcfd[1], F_SETFL, flags);
+	
+	write(1, "Forking a process\n",18);
+	pid_t cpid = fork();
+
+	if (cpid < 0) {
+		perror("Fork");
+	} else if (cpid == 0) {
+		write(1, "Child Process\n", 14);
+		char buff[19];
+		close(pcfd[1]);
+		if (read(pcfd[0], buff, 19) < 0)
+			perror("Child Read");
+	//Setting O_NONBLOCK gives "Resource temporarily unavailable"
+		else
+			printf("%s was read from parent\n", buff);
+		close(pcfd[0]);
+	} else {
+		wait(NULL);
+		if (!write(1, "Parent Process\n", 15))
+			perror("write");
+		close(pcfd[0]);  //Close read end of pipe
+		write(pcfd[1], "System Programming\0", 19);
+		close(pcfd[1]); //Close write end of pipe
+	}
+	
+
+	return 0;
+}
