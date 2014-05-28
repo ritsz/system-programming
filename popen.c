c7189405699e58621cec881646e056910459b4f5
Ritesh Ranjan <r.ranjan789@gmail.com>
2014-05-29 01:34:13 +0530

    Popen/Pclose

---
+++ popen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2) {
+		write(STDOUT_FILENO, "Incorrect Usage\n", 16);
+		_exit(-1);
+	}
+	FILE *file = popen( argv[1], "r");
+	if (file == NULL)
+		perror("popen");
+
+	int fd = fileno(file);
+	int len = 0;
+	char buff[256];
+
+	while ((len = read(fd, buff, 256)) > 0) {
+		write(STDOUT_FILENO, buff, len);
+	}
+	pclose(file);
+}
