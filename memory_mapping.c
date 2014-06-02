683229b293f8992d28aa1050e752d5ce2ad7408b
Ritesh Ranjan <r.ranjan789@gmail.com>
2014-06-03 00:26:47 +0530

    mmap basics

---
+++ memory_mapping.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+
+int main(void)
+{
+	int fd;
+	if ((fd = open("testfile.txt", O_RDWR)) < 0)
+		perror("Open");
+	
+	char *buff;
+	/*
+	 * Creating a private Copy-on-Write mapping of file. Any updates to this
+	 * structure affects only the private memory and doesn't sync on the
+	 * file.
+	 */
+	if ((buff = mmap(NULL, 512*1024, PROT_READ|PROT_WRITE, MAP_PRIVATE, fd, 0))
+			== MAP_FAILED)
+		perror("mmap");
+
+	printf("%c%c%c\n", *buff, *(buff + 1), *(buff + 2));
+	buff[0] = 'S';
+	printf("%c%c%c\n", *buff, *(buff + 1), *(buff + 2));
+	/* Only memory value was changed, file doesn't get affected even if
+	 * msync is tried
+	 */
+
+	char *shared_buff;
+	/* Creating a shared mapping of file. Any changes made to this mapping,
+	 * are reflected in all processes sharing this memory map. The changes
+	 * affect the file as well. But msync should be called to make sure.
+	 * Else all data are finally synced at munmap
+	 */
+	if ((shared_buff = mmap(NULL, 512*1024, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0))
+			== MAP_FAILED)
+		perror("mmap");
+	
+	printf("%c%c%c\n", *shared_buff, *(shared_buff + 1), *(shared_buff + 2));
+	*(shared_buff + 1) = 'I';
+	printf("%c%c%c\n", *shared_buff, *(shared_buff + 1), *(shared_buff + 2));
+
+	if (msync(shared_buff, 512*1024, MS_SYNC) < 0)
+		perror("msync");
+	if (munmap(shared_buff, 512*1024) < 0)
+		perror("Unmap");
+
+	return 0;
+}
