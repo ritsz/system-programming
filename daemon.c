d95132071192f35c3cad2eadb6c3b0d4d204f6af
Ritesh Ranjan <r.ranjan789@gmail.com>
2014-06-06 20:26:53 +0530

    Daemons with signal handlers

--- daemon.c
+++ daemon.c
@@ -3,6 +3,35 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <linux/fs.h>
+#include <signal.h>
+#include <stdio.h>
+
+void mysigint()
+{
+	write(STDOUT_FILENO, "Caught the SIGINT signal!\n", 26);
+	return;    
+} 
+
+/* Our own SIGKILL handler */
+void mysigkill()
+{
+	write(STDOUT_FILENO, "Caught the SIGKILL signal!\n", 27);
+	return;    
+} 
+
+/* Our own SIGHUP handler */
+void mysighup()
+{
+	write(STDOUT_FILENO, "Caught the SIGHUP signal!\n", 26);
+	return;    
+} 
+
+/* Our own SIGTERM handler */
+void mysigterm()
+{
+	write(STDOUT_FILENO, "Caught the SIGTERM signal!\n", 27);
+	return;    
+}
 
 int main(void)
 {
@@ -42,12 +71,22 @@ int main(void)
 	close(STDIN_FILENO);
 	close(STDOUT_FILENO);
 
+	/* Duplicate the file fd over standard input, output and error */
 	dup(i);
 	dup(i);
+	dup(i);
+	/* Add signal handlers */
+	if (signal(SIGINT, mysigint) == SIG_ERR)
+       		perror("Cannot handle SIGINT!");        
+    	if (signal(SIGHUP, mysighup) == SIG_ERR)
+       		perror("Cannot handle SIGHUP!");  
+    	if (signal(SIGTERM, mysigterm) == SIG_ERR)
+       		perror("Cannot handle SIGTERM!"); 
+	if (signal(SIGKILL, mysigterm) == SIG_ERR)
+       		perror("Cannot handle SIGKILL!"); 
 
 	while (1) {
-		write(STDOUT_FILENO, "Logging Begins\n", 15);
+		write(STDOUT_FILENO, "Logging Daemon\n", 15);
 		sleep(5);
 	}
-
 }
