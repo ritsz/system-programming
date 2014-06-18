f4ce937b468df1d53348a18321a42555bc00712d
Ritesh Ranjan <r.ranjan789@gmail.com>
2014-06-18 15:55:49 +0530

    concurrent server with fork. Process per connection model

--- client.c
+++ client.c
@@ -11,55 +11,54 @@
 
 int main(int argc, char *argv[])
 {
-    int sockfd = 0, n = 0;
-    char recvBuff[1024];
-    struct sockaddr_in serv_addr; 
+	int sockfd = 0, n = 0;
+	char recvBuff[2048];
+	struct sockaddr_in serv_addr; 
 
-    if(argc != 2)
-    {
-        printf("\n Usage: %s <ip of server> \n",argv[0]);
-        return 1;
-    } 
+	if(argc != 3) {
+		printf("\n Usage: %s <ip of server> <file to read>\n",argv[0]);
+		return 1;
+	} 
 
-    memset(recvBuff, '0',sizeof(recvBuff));
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("\n Error : Could not create socket \n");
-        return 1;
-    } 
+	memset(recvBuff, '0',sizeof(recvBuff));
+	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("\n Error : Could not create socket \n");
+		return 1;
+	} 
 
-    memset(&serv_addr, '0', sizeof(serv_addr)); 
+	memset(&serv_addr, '0', sizeof(serv_addr)); 
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(5000); 
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(5000); 
 
-    if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0)
-    {
-        printf("\n inet_pton error occured\n");
-        return 1;
-    } 
+	if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0) {
+		printf("\n inet_pton error occured\n");
+		return 1;
+	} 
 
-    if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-       printf("\n Error : Connect Failed \n");
-       return 1;
-    } 
+	if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+		printf("\n Error : Connect Failed \n");
+		return 1;
+	} 
 
-    while ( (n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)
-    {
-        recvBuff[n] = 0;
-        if(fputs(recvBuff, stdout) == EOF)
-        {
-            printf("\n Error : Fputs error\n");
-        }
-    }
+	if ((write(sockfd, argv[2], 128)) < 0) {
+		perror("Socket write");
+		close(sockfd);
+		exit(-1);
+	}
 
-    close(sockfd);
+	while ( (n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0) {
+		recvBuff[n] = 0;
+		if(fputs(recvBuff, stdout) == EOF) {
+			printf("\n Error : Fputs error\n");
+		}
+	}
 
-    if(n < 0)
-    {
-        printf("\n Read error \n");
-    } 
+	close(sockfd);
 
-    return 0;
+	if(n < 0) {
+		printf("\n Read error \n");
+	} 
+
+	return 0;
 }
--- server.c
+++ server.c
@@ -8,10 +8,42 @@
 #include <string.h>
 #include <sys/types.h>
 #include <time.h> 
+#include <fcntl.h>
+
+int handle_connection(int sockd)
+{
+	int ret, fd;
+	char buff[2048];
+	/* Read the name of file to read */
+	if ((ret = read(sockd, buff, sizeof(buff))) < 0) {
+		perror("socket read");
+		exit(-1);
+	}
+	buff[ret] = '\0';
+
+	printf("Request for %s file received\n", buff);
+
+	if ((fd = open(buff, O_RDONLY)) < 0) {
+		perror("file open");
+		exit(-1);
+	}
+
+	if ((ret = read(fd, buff, 2048)) < 0) {
+		perror("file read");
+		exit(-1);
+	}
+
+	if ((ret = write(sockd, buff, ret)) < 0) {
+		perror("socket write");
+		exit(-1);
+	}
+	
+	return 0;
+}
 
 int main(int argc, char *argv[])
 {
-    int listenfd = 0, connfd = 0;
+    int listenfd = 0, connfd = 0, cpid;
     struct sockaddr_in serv_addr, cli_addr; 
 
     char sendBuff[1025];
@@ -40,10 +72,26 @@ int main(int argc, char *argv[])
 	printf("connection accepted from %s, port %d\n",
 			inet_ntop(AF_INET, &cli_addr.sin_addr, sendBuff,sizeof(sendBuff)),
 			ntohs(cli_addr.sin_port));
-        ticks = time(NULL);
-        snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
-        write(connfd, sendBuff, strlen(sendBuff)); 
+	
+	/* Adding support for concurrency. The connection that are accepted are
+	 * handelled by a forked child. The parent only closes the connfd, thus
+	 * bringing the reference count of the connfd to 1, which had become 2
+	 * post forking. Thus when child calls close itself, connfd refcount
+	 * goes to 0 and hence can be cleared. Also since reference count is 0,
+	 * the kernel will send a FIN and initiate a TCP 4 way handshake.
+	 */
+	cpid = fork();
+	if (cpid == 0) {
+		close(listenfd);
+		ticks = time(NULL);
+        	snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
+        	write(connfd, sendBuff, strlen(sendBuff));
+		if (handle_connection(connfd) < 0)
+			perror("handle_connection");
 
+		close(connfd);
+		exit(0);
+	}
         close(connfd);
      }
 }
