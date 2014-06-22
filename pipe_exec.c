b7d9e8d319e4b493173d018502fda30123d88577
Ritesh Ranjan <r.ranjan789@gmail.com>
2014-06-22 20:19:40 +0530

    testing exec and fd

--- pipe_exec.c
+++ pipe_exec.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+#define TEST_EXEC_FD 0
 
 int main(void)
 {
@@ -23,14 +27,31 @@ int main(void)
 					 STDOUT. Thus anything ls returns to
 					 STDOUT, goes to pfds[1] instead, which
 					 can be read at pfds[0]*/
-		execlp("ls", "ls", "-l", NULL); /* Execute ls command*/
+		execlp("ls", "ls", "-l", NULL); /* Execute ls command. Upon exec
+						 the text part is overlayed by
+						 new code. The file descriptors
+						 are copied across exec() calls.
+						 The data written to pipe
+						 pfds[1] is forwarded to
+						 pfds[0], which gets written to
+						 a file. If we don't do
+						 dup(pfds[1]), data will get
+						 written to STDOUT, thus showing
+						 that fd get copied on exec.*/
 	} else {
 		close(pfds[1]);		/*Not required for parent*/
-		close(STDIN_FILENO);	/*STDIN_FILENO is closed*/
-		dup(pfds[0]);		/*pfds[0] copied to STDIN, ie any data
-					  avaibale at pfds[0] is assumed to be
-					  from STDIN by wc command*/
-		execlp("wc", "wc", "-l", NULL);
+		if (!TEST_EXEC_FD) {
+			close(STDIN_FILENO);	/*STDIN_FILENO is closed*/
+			dup(pfds[0]);		/*pfds[0] copied to STDIN, ie any data
+						  avaibale at pfds[0] is assumed to be
+						  from STDIN by wc command*/
+			execlp("wc", "wc", "-l", NULL);
+		}
+		int fd = open("LOG", O_RDWR|O_CREAT);
+		char buff[2048];
+		int ret = read(pfds[0], buff, 2048);
+		write(fd, buff, ret);
+		close(fd);
 	}
 
 	return 0;
