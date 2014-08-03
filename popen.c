5b08607c384c58a8bc0f7ca7a4842288e347a070
Ritesh Ranjan <r.ranjan789@gmail.com>
2014-08-03 23:14:56 +0530

    handling write errors

--- popen.c
+++ popen.c
@@ -4,21 +4,29 @@
 int main(int argc, char* argv[])
 {
 	if (argc < 2) {
-		write(STDOUT_FILENO, "Incorrect Usage\n", 16);
+		write(STDOUT_FILENO, "Incorrect Usage\nGive a command as input\n", 40);
 		_exit(-1);
 	}
+	/*  The  popen()  function opens a process by creating a pipe, forking,
+	 *  and invoking the shell.  Since a pipe is by definition unidirectional, 
+	 *  the type argument may specify only reading or writing,not both.
+	 */
 	FILE *file = popen( argv[1], "r");
 	if (file == NULL) {
 		perror("popen");
 		_exit(-1);
 	}
-
+	
+	/* Examines a stream and returns its file descriptor*/
 	int fd = fileno(file);
 	int len = 0;
 	char buff[256];
 
 	while ((len = read(fd, buff, 256)) > 0) {
-		write(STDOUT_FILENO, buff, len);
+		if (write(STDOUT_FILENO, buff, len) < 0) {
+			perror("write");
+			_exit(-1);
+		}
 	}
 	pclose(file);
 }
