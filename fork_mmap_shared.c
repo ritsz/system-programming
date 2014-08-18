e6e8af84760656e014759916782ea2be37f792b5
Ritesh Ranjan <r.ranjan789@gmail.com>
2014-08-18 11:46:12 +0530

    correcting mistakes in shared file-backed mmap

--- fork_mmap_shared.c
+++ fork_mmap_shared.c
@@ -25,16 +25,17 @@ int main(void)
         }
         size_file = stat_buf.st_size;
 
+	if ((buff = mmap(NULL, size_file, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0))
+			== MAP_FAILED) {
+		perror("mmap");
+		_exit(-1);
+	}
+
 	pid = fork();
 	if (pid < 0) {
 		perror("Fork");
 		_exit(-1);
 	} else if (pid == 0) {
-		if ((buff = mmap(NULL, size_file, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0))
-				== MAP_FAILED) {
-			perror("mmap");
-			_exit(-1);
-		}
 		printf("CHILD %c%c%c\n", *buff, *(buff+1), *(buff+2));
 		buff[0] = 'D';
 		printf("CHILD %c%c%c\n", *buff, *(buff+1), *(buff+2));
@@ -44,11 +45,6 @@ int main(void)
 		}
 		_exit(0);
 	} else {
-		if ((buff = mmap(NULL, size_file, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0))
-				== MAP_FAILED) {
-			perror("mmap");
-			_exit(-1);
-		}
 		wait(-1);
 		printf("PARENT %c%c%c\n", *buff, *(buff+1), *(buff+2));
 	}
