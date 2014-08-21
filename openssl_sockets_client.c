ffbfb5cdb8cf3ed592c6aa1c6208611160b1141d
Ritesh Ranjan <r.ranjan789@gmail.com>
2014-08-21 17:38:31 +0530

    Documentation of first SSL encrypted socket client

--- openssl_sockets_client.c
+++ openssl_sockets_client.c
@@ -8,94 +8,110 @@
 #include <unistd.h>
 #include <errno.h>
 
-//#include <openssl/rand.h>
+#include <openssl/rand.h>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
+
 
 // Simple structure to keep track of the handle, and
 // of what needs to be freed later.
 typedef struct {
-    int socket;
-    SSL *sslHandle;
-    SSL_CTX *sslContext;
+	int socket;
+    	SSL *sslHandle;
+    	SSL_CTX *sslContext;
 } connection;
 
 // For this example, we'll be testing on openssl.org
-#define SERVER  "www.openssl.org"
+#define SERVER  "www.lwn.net"
 #define PORT 443
+
 
 // Establish a regular tcp connection
 int tcpConnect ()
 {
-  int error, handle;
-  struct hostent *host;
-  struct sockaddr_in server;
+  	int error, handle;
+  	struct hostent *host;
+  	struct sockaddr_in server;
 
-  host = gethostbyname (SERVER);
-  handle = socket (AF_INET, SOCK_STREAM, 0);
-  if (handle == -1)
-    {
-      perror ("Socket");
-      handle = 0;
-    }
-  else
-    {
-      server.sin_family = AF_INET;
-      server.sin_port = htons (PORT);
-      server.sin_addr = *((struct in_addr *) host->h_addr);
-      bzero (&(server.sin_zero), 8);
+  	if (!(host = gethostbyname (SERVER))) {
+		printf("Can't get hostname\n");
+		return 0; /*return zero handle*/
+	}
 
-      error = connect (handle, (struct sockaddr *) &server,
-                       sizeof (struct sockaddr));
-      if (error == -1)
-        {
-          perror ("Connect");
-          handle = 0;
-        }
-    }
+  	/* Create a socket */
+	handle = socket (AF_INET, SOCK_STREAM, 0);
+  	if (handle == -1 ) {
+      		perror ("Socket");
+      		handle = 0;
+    	} else {
+		/* Create a sockaddr structure with details about the host to
+		 * connect to.
+		 */
+      		server.sin_family = AF_INET;
+      		server.sin_port = htons (PORT);
+      		server.sin_addr = *((struct in_addr *) host->h_addr);
+      		bzero (&(server.sin_zero), 8);
+		
+		/* Connect to the specified host using the created socket */
+      		error = connect (handle, (struct sockaddr *) &server,
+                       	sizeof (struct sockaddr));
+      		if (error == -1) {
+          		perror ("Connect");
+          		handle = 0;
+        	}
+    	}
 
-  return handle;
+  	return handle;
 }
 
 // Establish a connection using an SSL layer
-connection *sslConnect (void)
-{
-  connection *c;
+connection *sslConnect (void) {
+  	connection *c;
 
-  c = malloc (sizeof (connection));
-  c->sslHandle = NULL;
-  c->sslContext = NULL;
+  	c = malloc (sizeof (connection));
+  	c->sslHandle = NULL;
+  	c->sslContext = NULL;
 
-  c->socket = tcpConnect ();
-  if (c->socket)
-    {
-      // Register the error strings for libcrypto & libssl
-      SSL_load_error_strings ();
-      // Register the available ciphers and digests
-      SSL_library_init ();
+	/* Set up a TCP session first. Once the TCP is up, we can establish a
+	 * SSL encrypted connection on top of that TCL connection. We use the
+	 * structure declared above to save TCP socket, sslContext and
+	 * sslHandle.
+	 * 	1> Declare a new context
+	 *	2> Create a new ssl handle in that context
+	 *	3> Attach ssl handle to the tcp socket
+	 *	4> Initiate SSL handshake.
+	 */
+  	c->socket = tcpConnect();
+  	if (c->socket) {
+      		// Register the error strings for libcrypto & libssl
+      		SSL_load_error_strings ();
+      		// Register the available ciphers and digests
+      		SSL_library_init ();
 
-      // New context saying we are a client, and using SSL 2 or 3
-      c->sslContext = SSL_CTX_new (SSLv23_client_method ());
-      if (c->sslContext == NULL)
-        ERR_print_errors_fp (stderr);
+      		// New context saying we are a client, and using SSL 2 or 3
+      		c->sslContext = SSL_CTX_new (SSLv23_client_method ());
+      		if (c->sslContext == NULL)
+        		ERR_print_errors_fp (stderr);
 
-      // Create an SSL struct for the connection
-      c->sslHandle = SSL_new (c->sslContext);
-      if (c->sslHandle == NULL)
-        ERR_print_errors_fp (stderr);
+      		/* Create an SSL struct for the connection using the client
+		 * context we just created.
+		 */
+      		c->sslHandle = SSL_new (c->sslContext);
+     		if (c->sslHandle == NULL)
+       	 		ERR_print_errors_fp (stderr);
 
-      // Connect the SSL struct to our connection
-      if (!SSL_set_fd (c->sslHandle, c->socket))
-        ERR_print_errors_fp (stderr);
+      		// Connect the SSL struct to our connection
+      		if (!SSL_set_fd (c->sslHandle, c->socket))
+        		ERR_print_errors_fp (stderr);
 
-      // Initiate SSL handshake
-      if (SSL_connect (c->sslHandle) != 1)
-        ERR_print_errors_fp (stderr);
-    }
-  else
-    {
-      perror ("Connect failed");
-    }
+     		// Initiate SSL handshake
+      		if (SSL_connect (c->sslHandle) != 1)
+        		ERR_print_errors_fp (stderr);
+
+		printf("SSL Handshake established. Data will now be encrypted\n");
+    	} else {
+      		printf("TCP connection failed.\n");
+    	}
 
   return c;
 }
@@ -103,17 +119,16 @@ connection *sslConnect (void)
 // Disconnect & free connection struct
 void sslDisconnect (connection *c)
 {
-  if (c->socket)
-    close (c->socket);
-  if (c->sslHandle)
-    {
-      SSL_shutdown (c->sslHandle);
-      SSL_free (c->sslHandle);
-    }
-  if (c->sslContext)
-    SSL_CTX_free (c->sslContext);
+  	if (c->socket)
+    		close (c->socket);
+  	if (c->sslHandle) {
+      		SSL_shutdown (c->sslHandle);
+      		SSL_free (c->sslHandle);
+    	}
+  	if (c->sslContext)
+    		SSL_CTX_free (c->sslContext);
 
-  free (c);
+  	free (c);
 }
 
 // Read all available text from the connection
@@ -125,22 +140,27 @@ char *sslRead (connection *c)
   	char buffer[1024];
 
   	if (c) {
-      	while (1) {
-          	if (!rc)
-            		rc = malloc (readSize * sizeof (char) + 1);
-          	else
-            		rc = realloc (rc, (count + 1) *
-                        readSize * sizeof (char) + 1);
+      		while (1) {
+			/* Allocate 1024 bytes to rc or add 1024 bytes if rc not NULL
+			 */
+          		if (!rc)
+            			rc = malloc (readSize * sizeof (char) + 1);
+          		else
+            			rc = realloc (rc, (count + 1)*readSize*sizeof(char)+1);
 
-          		received = SSL_read (c->sslHandle, buffer, readSize);
+          		/* Read data on the sslHandle and save to a buffer */
+			received = SSL_read (c->sslHandle, buffer, readSize);
           		buffer[received] = '\0';
-
+			
+			/* Add buffer to end of rc and if read size if less than
+			 * 1024, then EOF must have been reached.
+			 */
           		if (received > 0)
             			strcat (rc, buffer);
-
-          		if (received < readSize)
-            			break;
-         	 	count++;
+       			if (received < readSize)
+     				break;
+      	 		
+			count++;
         	}
     	}
 
@@ -160,15 +180,16 @@ int main (int argc, char **argv)
   	connection *c;
   	char *response;
 
-  	c = sslConnect ();
+  	c = sslConnect();
+	if (c->sslHandle) {
+		sslWrite (c, "GET /\r\n\r\n");
+  		response = sslRead (c);
 
-  	sslWrite (c, "GET /\r\n\r\n");
-  	response = sslRead (c);
-
-	printf ("%s\n", response);
+		printf ("%s\n", response);
 	
-  	sslDisconnect (c);
-  	free (response);
+  		sslDisconnect (c);
+  		free (response);
+	}
 	
   	return 0;
 }
