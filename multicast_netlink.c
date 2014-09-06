ca06e81d10028512c9ec1e05c75adacc8eb30f7f
Ritesh Ranjan <r.ranjan789@gmail.com>
2014-09-06 23:39:57 +0530

    Adding O_NONBLOCK flag on socket

--- multicast_netlink.c
+++ multicast_netlink.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/fcntl.h>
 #include <sys/socket.h>
 #include <linux/netlink.h>
 
 #define NETLINK_USER 31
-#define MULTICAST_GROUP 1
+#define MULTICAST_GROUP 0x21
 #define MAX_PAYLOAD 1024
 
 struct sockaddr_nl src_addr, dest_addr;
@@ -13,7 +16,7 @@ struct iovec iov;
 int sock_fd;
 struct msghdr msg;
 
-void main()
+int main()
 {
 	sock_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_USER);
 	if (sock_fd < 0) {
@@ -21,8 +24,11 @@ void main()
 		return -1;
 	}
 
+	fcntl(sock_fd, F_SETFL, O_NONBLOCK | fcntl(sock_fd, F_GETFL));
+
 	memset(&src_addr, 0, sizeof(src_addr));
 	src_addr.nl_family = AF_NETLINK;
+	src_addr.nl_groups = MULTICAST_GROUP;
 	src_addr.nl_pid = getpid();
 
 	bind(sock_fd, (struct sockaddr *)&src_addr, sizeof(src_addr));
@@ -47,9 +53,15 @@ void main()
 	msg.msg_iovlen = 1;
 
 	printf("Sending message to kernel\n");
-	sendmsg(sock_fd, &msg, 0);
+	if (sendmsg(sock_fd, &msg, 0) < 0) {
+		perror("sendmsg");
+		return -1;
+	}
 	
-	recvmsg(sock_fd, &msg, 0);
+	if (recvmsg(sock_fd, &msg, 0) < 0) {
+		perror("recvmesg");
+		return -1;
+	}
 	printf("Received message payload: %s\n", NLMSG_DATA(nlh));
 	close(sock_fd);
 }
