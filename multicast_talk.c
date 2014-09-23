a3eb5111cfce59f1d6a88dc83d8f1ac0e9bc84b3
Ritesh Ranjan <r.ranjan789@gmail.com>
2014-09-23 18:09:48 +0530

    Adding config values

--- multicast_talk.c
+++ multicast_talk.c
@@ -2,6 +2,16 @@
 #include <sys/socket.h>
 #include <linux/netlink.h>
 
+#define NETLINK_USER 31
+#define MULTICAST_GROUP 3
+#define MAX_PAYLOAD 1024 /* maximum payload size*/
+
+struct sockaddr_nl src_addr, dest_addr; 
+struct nlmsghdr *nlh;
+struct iovec iov;
+int sock_fd;
+struct msghdr msg;
+
 int main()
 {
 
